e9e60f19c670dc8b9048956775fb050a3333ef01
vladp26 <95245952+vladp26@users.noreply.github.com>
2024-12-15 10:14:34 +0200

    Add files via upload

---
+++ tests.py
@@ -0,0 +1,23 @@
+import unittest
+from functions import *
+
+class Test(unittest.TestCase):
+    def setUp(self):
+        self.a = 20
+        self.b = 10
+
+    def test_add(self):
+        result = add(self.a, self.b)
+        self.assertEqual(result, 30)
+
+    def test_sub(self):
+        result = subtract(self.a, self.b)
+        self.assertEqual(result, 10)
+
+    def test_mul(self):
+        result = multiply(self.a, self.b)
+        self.assertEqual(result, 200)
+
+    def test_div(self):
+        result = divide(self.a, self.b)
+        self.assertEqual(result, 2)
